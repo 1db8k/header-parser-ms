03782fca190346290fa386bed0d7fe0ef8cbe889
Your Name <you@example.com>
2016-12-19 00:28:37 +0100

    basic server construct

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
 const express = require('express')
 const app = express()
+
+app.use('/', express.static(__dirname + '/public'))
+
+// app.get('/api/whoami', (req, res) => {
+//   res.json({
+//     ipaddress: req.ip,
+//     language: null,
+//     operating_system: null
+//   })
+// })
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Example app listening on port 5000!')
+})
