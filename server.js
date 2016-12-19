b08b8e4320fdc19e6904b6e1653228f3858a47cb
Your Name <you@example.com>
2016-12-19 01:07:44 +0100

    added object functionality

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const app = express()
-
+const util = require('util')
 app.use('/', express.static(__dirname + '/public'))
 
-// app.get('/api/whoami', (req, res) => {
-//   res.json({
-//     ipaddress: req.ip,
-//     language: null,
-//     operating_system: null
-//   })
-// })
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    ipaddress: req.ip,
+    language: req.acceptsLanguages()[0],
+    operating_system: util.inspect(req, true, null)
+  })
+})
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Example app listening on port 5000!')
